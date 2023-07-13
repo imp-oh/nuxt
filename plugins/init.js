d9272a38c182fe07c120c3257bb75b01eba10088
ppfv <ppfv@qq.com>
2023-07-13 20:35:08 +0800

    init

--- plugins/init.js
+++ plugins/init.js
@@ -3,15 +3,12 @@
  * 用于页面获取
  */
 
-import { nextTick } from 'vue'
 import { defineNuxtPlugin } from "../core/nuxt.js"
 
 export default defineNuxtPlugin({
   name: "nuxt:init",
   enforce: "pre",
   async setup (nuxtApp) {
-    const done = nuxtApp.deferHydration()
-    nextTick(done)
     return nuxtApp
   }
 })
